35954b3516ebf81cd22ab3cc62d6b51aaef791f3
Naash <hasan.taskin.pro@gmail.com>
2018-04-19 10:27:23 +0200

    Upload fonctionelle

--- app/db.php
+++ app/db.php
@@ -1,6 +1,6 @@
 <?php
 // RASPBERRY IP : 192.168.8.132
-define("APP_DB_HOST","192.168.8.132");
+define("APP_DB_HOST","192.168.8.122");
 define("APP_DB_NAME","Wooamye");
 define("APP_DB_USER","root");
 define("APP_DB_PWD", "password");
